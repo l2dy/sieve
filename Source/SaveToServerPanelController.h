394f6de2bc59f952e0c7c1bd6b53e8664bc0584d
dergraf83 <dergraf83@localhost>
2010-08-18 16:10:00 +0200

    Added panel for asking the name of a document on the server

---
+++ Source/SaveToServerPanelController.h
@@ -0,0 +1,25 @@
+//
+//  SaveToServerPanelController.h
+//  Sieve
+//
+//  Created by Sven Weidauer on 18.08.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+typedef void (^SaveCompletionBlock)( NSInteger returnCode, NSString *name );
+
+@interface SaveToServerPanelController : NSWindowController {
+    NSString *result;
+}
+
+@property (readwrite, copy) NSString *result;
+
+- (IBAction) saveClicked: (id) sender;
+- (IBAction) cancelClicked: (id) sender;
+
+- (void) beginSheetModalForWindow: (NSWindow *) window completionBlock: (SaveCompletionBlock) block;
+
+@end
---
+++ Source/SaveToServerPanelController.m
@@ -0,0 +1,49 @@
+//
+//  SaveToServerPanelController.m
+//  Sieve
+//
+//  Created by Sven Weidauer on 18.08.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SaveToServerPanelController.h"
+
+
+@implementation SaveToServerPanelController
+
+@synthesize result;
+
+- init;
+{
+    self = [super initWithWindowNibName: @"SaveToServerPanel"];
+    [self setResult: @""];
+    return self;
+}
+
+- (IBAction) saveClicked: (id) sender;
+{
+    [NSApp endSheet: [self window] returnCode: NSOKButton];
+}
+
+- (IBAction) cancelClicked: (id) sender;
+{
+    [NSApp endSheet: [self window] returnCode: NSCancelButton];
+}
+
+- (void)sheetDidEnd:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo;
+{
+    [sheet orderOut: self];
+    SaveCompletionBlock block = (SaveCompletionBlock) contextInfo;
+    if (nil != block) {
+        block( returnCode, [self result] );
+        [block release];
+    }
+}
+
+- (void) beginSheetModalForWindow: (NSWindow *) window completionBlock: (SaveCompletionBlock) block;
+{
+    [NSApp beginSheet: [self window] modalForWindow: window modalDelegate: self 
+       didEndSelector:@selector(sheetDidEnd:returnCode:contextInfo:) contextInfo: (void *)[block copy] ];
+}
+
+@end
